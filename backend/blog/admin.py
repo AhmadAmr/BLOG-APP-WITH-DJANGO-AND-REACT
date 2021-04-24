a90edf113ba5e59e4771c78c31ccd951beb854b7
AhmadAmr <ahmedamr18841@gmail.com>
2021-04-24 05:19:08 +0200

    add models

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BlogPost,Categories
 
-# Register your models here.
+class BlogPostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content', )
+
+
+
+
+admin.site.register(Categories)
+admin.site.register(BlogPost,BlogPostAdmin)
+
+
--- backend/blogy/settings.py
+++ backend/blogy/settings.py
@@ -143,4 +143,6 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 SUMMERNOTE_THEME = 'bs4'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
