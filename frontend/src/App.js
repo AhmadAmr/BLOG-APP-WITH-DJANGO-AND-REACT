e8dea6900a5800b7f6fb52c5569c6be3907683d5
AhmadAmr <ahmedamr18841@gmail.com>
2021-04-27 06:35:27 +0200

    Add frontend components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+import Layout from "./hocs/Layout";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import BlogDetail from "./components/BlogDetail";
+import Category from "./components/Category";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/blog" component={Blog} />
+            <Route exact path="/category/:id" component={Category} />
+            <Route exact path="/blog/:id" component={BlogDetail} />
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ frontend/src/components/BlogDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BlogDetail = () => {
+
+return (
+    <h1>BlogDetail</h1>
+)
+
+
+}; export default BlogDetail;
